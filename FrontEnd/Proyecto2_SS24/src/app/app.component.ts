7b48c7826b241a7b58cd956fe5907440382a3296
David-G-Cotom <brandoncotom202231460@cunoc.edu.gt>
2024-10-05 21:56:51 -0600

    Agregando los Componentes y Servicios que se Empezaran a Utilizar en la parte de LogIn para la Primera Iteracion.

--- FrontEnd/Proyecto2_SS24/src/app/app.component.ts
+++ FrontEnd/Proyecto2_SS24/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegistroUsuarioComponent } from "./components/forms/registro-usuario/registro-usuario.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RegistroUsuarioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
